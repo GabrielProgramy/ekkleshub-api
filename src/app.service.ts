1f760a7d79e0166f26b67fd891220433c7dc7cb3
Gabriel Costa <gabrielestevan14@gmail.com>
2026-08-04 15:55:54 -0300

    feat: adicionando o endpoint health

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHealthAPI(): object {
+    return {
+      status: 'ok',
+      service: 'ekkleshub-api',
+    };
   }
 }
