e0fe5e0de6f69089474444c755d78e79eae9fc2d
Gabriel Costa <gabrielestevan14@gmail.com>
2026-08-10 18:22:47 -0300

    feat: implementar entidade Church

---
+++ src/church/entities/address.embedded.ts
@@ -0,0 +1,16 @@
+import { Column } from 'typeorm';
+
+export class Address {
+	@Column()
+	street: string;
+	@Column()
+	number: string;
+	@Column()
+	city: string;
+	@Column()
+	state: string;
+	@Column({ type: 'varchar', nullable: true })
+	complement: string | null;
+	@Column()
+	zip_code: string;
+}
