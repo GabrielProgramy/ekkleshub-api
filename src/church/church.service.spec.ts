adc8bab03ae9c09cc7da11fe60202edd02bc6a3a
Gabriel Costa <gabrielestevan14@gmail.com>
2026-08-13 11:19:07 -0300

    test: adicionar testes do ChurchService

--- src/church/church.service.spec.ts
+++ src/church/church.service.spec.ts
@@ -1,18 +1,153 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { ChurchService } from './church.service';
+import { Church, ChurchStatus, ChurchType } from './entities/church.entity';
+import { CreateChurchDto } from './dto/create-church.dto';
+import { ConflictException, NotFoundException } from '@nestjs/common';
+import { UpdateChurchDto } from './dto/update-church.dto';
 
 describe('ChurchService', () => {
-  let service: ChurchService;
+	let service: ChurchService;
+	let mockChurchRepository: {
+		exists: jest.Mock;
+		save: jest.Mock;
+		find: jest.Mock;
+		findOne: jest.Mock;
+		merge: jest.Mock;
+	};
+	let churchTestData: CreateChurchDto;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ChurchService],
-    }).compile();
+	let existingChurchTest: Church;
 
-    service = module.get<ChurchService>(ChurchService);
-  });
+	beforeEach(async () => {
+		mockChurchRepository = {
+			exists: jest.fn(),
+			save: jest.fn(),
+			find: jest.fn(),
+			findOne: jest.fn(),
+			merge: jest.fn(),
+		};
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+		churchTestData = {
+			name: 'Igreja Teste Sede',
+			address: {
+				city: 'São Paulo',
+				number: '20',
+				street: 'Rua Alguma coisa',
+				state: 'SP',
+				zip_code: '00022001',
+			},
+			type: ChurchType.HEADQUARTERS,
+		};
+
+		existingChurchTest = {
+			...churchTestData,
+			id: 'uuid-teste',
+			email: null,
+			phone: null,
+			status: ChurchStatus.ACTIVE,
+			created_at: new Date(),
+			updated_at: new Date(),
+		};
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [
+				ChurchService,
+				{
+					provide: getRepositoryToken(Church),
+					useValue: mockChurchRepository,
+				},
+			],
+		}).compile();
+
+		service = module.get<ChurchService>(ChurchService);
+	});
+
+	it('deve impedir a criação de uma segunda igreja matriz', async () => {
+		mockChurchRepository.exists.mockResolvedValue(true);
+
+		await expect(service.create(churchTestData)).rejects.toThrow(
+			ConflictException,
+		);
+	});
+
+	it('deve permitir a criação de uma igreja matriz', async () => {
+		mockChurchRepository.exists.mockResolvedValue(false);
+
+		mockChurchRepository.save.mockResolvedValue({
+			...churchTestData,
+			id: 'uuid-teste',
+			status: ChurchStatus.ACTIVE,
+			create_at: new Date(),
+			updated_at: new Date(),
+		});
+
+		const result = await service.create(churchTestData);
+
+		expect(result.type).toBe(ChurchType.HEADQUARTERS);
+		expect(result.status).toBe(ChurchStatus.ACTIVE);
+		expect(mockChurchRepository.save).toHaveBeenCalled();
+	});
+
+	it('deve emitir erro caso não encontre uma igreja', async () => {
+		mockChurchRepository.findOne.mockResolvedValue(null);
+
+		await expect(service.findOneOrFail('id')).rejects.toThrow(
+			NotFoundException,
+		);
+	});
+
+	it('deve encontrar uma igreja pelo seu id', async () => {
+		const insertedChurch = {
+			...churchTestData,
+			status: ChurchStatus.ACTIVE,
+			created_at: new Date(),
+			updated_at: new Date(),
+			id: 'uuid-teste',
+		};
+
+		mockChurchRepository.findOne.mockResolvedValue(insertedChurch);
+
+		const result = await service.findOneOrFail('uuid-teste');
+
+		expect(result).toEqual(insertedChurch);
+	});
+
+	it('deve permitir a atualização de dados de uma igreja', async () => {
+		const updatedDataChurch = {
+			email: 'churchteste@email.com',
+		};
+
+		const mergedDataChurch = {
+			...existingChurchTest,
+			...updatedDataChurch,
+		};
+
+		mockChurchRepository.findOne.mockResolvedValue(existingChurchTest);
+		mockChurchRepository.merge.mockImplementation(
+			(existing: Church, updated: UpdateChurchDto) => {
+				Object.assign(existing, updated);
+
+				return existing;
+			},
+		);
+		mockChurchRepository.save.mockResolvedValue(mergedDataChurch);
+
+		const result = await service.update('uuid-teste', updatedDataChurch);
+
+		expect(result).toEqual(mergedDataChurch);
+		expect(mockChurchRepository.merge).toHaveBeenCalledWith(
+			existingChurchTest,
+			updatedDataChurch,
+		);
+		expect(mockChurchRepository.save).toHaveBeenCalledWith(existingChurchTest);
+	});
+
+	it('deve alterar o status da igreja para CLOSED', async () => {
+		mockChurchRepository.findOne.mockResolvedValue(existingChurchTest);
+
+		await service.close('uuid-teste');
+
+		expect(existingChurchTest.status).toEqual(ChurchStatus.CLOSED);
+		expect(mockChurchRepository.save).toHaveBeenCalledWith(existingChurchTest);
+	});
 });
