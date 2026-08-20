264f1383c7c6f7867b50f9b3d63d3591cd1610ca
Gabriel Costa <gabrielestevan14@gmail.com>
2026-08-20 18:17:08 -0300

    feat: registrar módulo de perfis de acesso

--- src/access-profile/access-profile.module.ts
+++ src/access-profile/access-profile.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AccessProfileController } from './access-profile.controller';
 import { AccessProfileService } from './access-profile.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccessProfile } from './entities/access-profile.entity';
 
 @Module({
-	imports: [],
+	imports: [TypeOrmModule.forFeature([AccessProfile])],
 	controllers: [AccessProfileController],
 	providers: [AccessProfileService],
 })
