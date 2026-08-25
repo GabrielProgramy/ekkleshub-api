db5fefc428cda7fc42c896cbe071560c25b32c7e
Gabriel Costa <gabrielestevan14@gmail.com>
2026-08-25 13:46:25 -0300

    feat: iniciar módulo de usuários

---
+++ src/users/entities/users.entity.ts
@@ -0,0 +1,43 @@
+import {
+	Column,
+	CreateDateColumn,
+	Entity,
+	PrimaryGeneratedColumn,
+	UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Users {
+	@PrimaryGeneratedColumn('uuid')
+	id: string;
+
+	@Column()
+	name: string;
+
+	@Column()
+	email: string;
+
+	@Column()
+	password: string;
+
+	@Column()
+	status: string;
+
+	@Column()
+	access_profile_id: string;
+
+	@Column()
+	church_id?: string;
+
+	@Column()
+	member_id?: string;
+
+	@Column()
+	last_access_at?: Date;
+
+	@CreateDateColumn()
+	createdAt?: Date;
+
+	@UpdateDateColumn()
+	updatedAt?: Date;
+}
---
+++ src/users/users.service.spec.ts
@@ -0,0 +1,151 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Users } from './entities/users.entity';
+import { AccessProfileService } from '../access-profile/access-profile.service';
+
+describe('UsersService', () => {
+	let service: UsersService;
+	let mockUsersRepository: {
+		exists: jest.Mock;
+		save: jest.Mock;
+		find: jest.Mock;
+		findOne: jest.Mock;
+		merge: jest.Mock;
+	};
+	let userData: Users;
+
+	let mockAccessProfileService: {
+		findOneOrFail: jest.Mock;
+	};
+
+	beforeEach(async () => {
+		mockUsersRepository = {
+			exists: jest.fn(),
+			save: jest.fn(),
+			find: jest.fn(),
+			findOne: jest.fn(),
+			merge: jest.fn(),
+		};
+
+		mockAccessProfileService = {
+			findOneOrFail: jest.fn(),
+		};
+
+		userData = {
+			id: 'uuid-teste',
+			name: 'John Doe',
+			email: 'johndoe@email.com',
+			password: 'hash_senha',
+			access_profile_id: 'uuid-access-profile',
+			status: 'ACTIVE',
+			createdAt: new Date(),
+			updatedAt: new Date(),
+		};
+
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [
+				UsersService,
+				{
+					provide: getRepositoryToken(Users),
+					useValue: mockUsersRepository,
+				},
+				{
+					provide: AccessProfileService,
+					useValue: mockAccessProfileService,
+				},
+			],
+		}).compile();
+
+		service = module.get<UsersService>(UsersService);
+	});
+
+	it('deve ser possível cadastrar um usuário com perfil válido', async () => {
+		const userDataCreated = {
+			name: 'John Doe',
+			email: 'johndoe@email.com',
+			password: '12345678',
+			access_profile: 'uuid-admin',
+		};
+
+		mockAccessProfileService.findOneOrFail.mockResolvedValue({
+			id: 'uuid-admin',
+			name: 'ADMIN',
+		});
+
+		mockUsersRepository.exists.mockResolvedValue(false);
+
+		mockUsersRepository.save.mockResolvedValue({
+			...userDataCreated,
+			id: 'uuid-admin',
+			status: 'ACTIVE',
+			createdAt: new Date(),
+			updatedAt: new Date(),
+		});
+
+		const result = await service.create(userDataCreated);
+
+		expect(result.status).toBe('ACTIVE');
+		expect(mockUsersRepository.exists).toHaveBeenCalledWith({
+			where: { email: 'johndoe@email.com' },
+		});
+		expect(mockAccessProfileService.findOneOrFail).toHaveBeenCalledWith(
+			'uuid-user',
+		);
+		expect(mockUsersRepository.save).toHaveBeenCalled();
+	});
+	it('deve ser possível buscar um usuário pelo seu id', async () => {
+		mockUsersRepository.findOne.mockResolvedValue(userData);
+
+		const result = await service.findOneOrFail('uuid-teste');
+
+		expect(result).toEqual(userData);
+		expect(mockUsersRepository.findOne).toHaveBeenCalledWith('uuid-teste');
+	});
+
+	it('deve listar todos os usuários cadastrados', async () => {
+		mockUsersRepository.find.mockResolvedValue([userData]);
+
+		const result = await service.findAll();
+
+		expect(result).toEqual([userData]);
+	});
+
+	it('deve ser possível atualizar um usuário', async () => {
+		const updatedDataUser = {
+			email: 'johndoe2@email.com',
+		};
+
+		const mergedDataUser = {
+			...userData,
+			...updatedDataUser,
+		};
+
+		mockUsersRepository.findOne.mockResolvedValue(userData);
+		mockUsersRepository.merge.mockImplementation((existing: Users, updated) => {
+			Object.assign(existing, updated);
+
+			return existing;
+		});
+
+		mockUsersRepository.save.mockResolvedValue(mergedDataUser);
+
+		const result = await service.update('uuid-teste', updatedDataUser);
+
+		expect(result).toEqual(mergedDataUser);
+		expect(mockUsersRepository.merge).toHaveBeenCalledWith(
+			userData,
+			updatedDataUser,
+		);
+		expect(mockUsersRepository.save).toHaveBeenCalledWith(mergedDataUser);
+	});
+
+	it('deve ser possível inativar um usuário', async () => {
+		mockUsersRepository.findOne.mockResolvedValue(userData);
+
+		await service.inactiveUser('uuid-teste');
+
+		expect(userData.status).toEqual('INACTIVE');
+		expect(mockUsersRepository.save).toHaveBeenCalledWith(userData);
+	});
+});
